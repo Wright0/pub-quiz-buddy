827b1cb10ccb233de6ad79e71b3355c8a429ccda
Luca Sanz Charreun <lucasanz2471@gmail.com>
2020-01-27 14:25:59 +0000

    added search by day service file

--- client/src/services/PubQuizzesService.js
+++ client/src/services/PubQuizzesService.js
@@ -1,8 +1,13 @@
-const baseURL = 'http://localhost:3000/api/pubQuizzes/'
+const baseURL = 'http://localhost:3000/api/pubQuizzes'
 
 export default {
   getQuizzes() {
     return fetch(baseURL)
+      .then(res => res.json())
+  }
+
+  getQuizzesByDay(day) {
+    return fetch(`${baseURL}/day/${day}`)
       .then(res => res.json())
   }
 }
